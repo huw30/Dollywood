e97dc5e2fec92ddfcd056249a29502abf611255c
Raina Wang <raina@weebly.com>
2017-09-12 21:10:51 -0700

    Initial Commit

---
+++ Flicks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flicks
+//
+//  Created by Raina Wang on 9/12/17.
+//  Copyright © 2017 Raina Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FlicksTests/FlicksTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlicksTests.swift
+//  FlicksTests
+//
+//  Created by Raina Wang on 9/12/17.
+//  Copyright © 2017 Raina Wang. All rights reserved.
+//
+
+import XCTest
+@testable import Flicks
+
+class FlicksTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
