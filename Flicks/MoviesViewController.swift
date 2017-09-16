b06d1d78843850dade09cb9e165696081a0394cc
Raina Wang <raina@weebly.com>
2017-09-15 23:31:24 -0700

    Add search bar

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 import AFNetworking
 import MBProgressHUD
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var movieSearchBar: UISearchBar!
 
     var movies: [[String: Any]] = [[String: Any]]()
     var alertView: AlertViewController!
     var refreshControl: UIRefreshControl!
+    var filtered:[[String: Any]] = [[String: Any]]()
+    var searchActive : Bool = false
+    var searchNoResult: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +28,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Do any additional setup after loading the view, typically from a nib.
         tableView.dataSource = self
         tableView.delegate = self
+        movieSearchBar.delegate = self
 
         refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
@@ -33,13 +38,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(searchActive) {
+            return filtered.count
+        }
         return movies.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        let movie: [String: Any]
 
-        let movie = movies[indexPath.row]
+        if(searchActive){
+            movie = filtered[indexPath.row]
+        } else {
+            movie = movies[indexPath.row]
+        }
+
         let title = movie["title"] as? String
         let synopsis = movie["overview"] as? String
         var posterURL: URL!
@@ -55,6 +69,24 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.movie = movie
 
         return cell
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        var numOfSections: Int = 0
+        if (searchActive && filtered.count == 0) {
+            let noDataLabel: UILabel     = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
+            noDataLabel.text          = "No result found"
+            noDataLabel.textColor     = UIColor.black
+            noDataLabel.textAlignment = .center
+            tableView.backgroundView  = noDataLabel
+            tableView.separatorStyle  = .none
+        } else {
+            tableView.separatorStyle = .singleLine
+            numOfSections            = 1
+            tableView.backgroundView = nil
+        }
+        return numOfSections
     }
     func refreshControlAction(_ refreshControl: UIRefreshControl) {
         fetchMovies()
@@ -87,8 +119,34 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                     self.refreshControl.endRefreshing()
                 }
             }
-        });
+        })
+
         task.resume()
+    }
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchActive = true
+    }
+
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchActive = false
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filtered = searchText.isEmpty ? movies : movies.filter({ (movie) -> Bool in
+            let movieTitle = movie["title"] as! String
+            let movieOverview = movie["overview"] as! String
+            let titleRange = movieTitle.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+            let overviewRange = movieOverview.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+            return titleRange || overviewRange
+        })
+        self.tableView.reloadData()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -111,6 +169,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         if let alertView = alertView {
             alertView.dismiss(animated: true, completion: nil)
         }
+    }
+    func showNoResult() {
+        let noDataLabel: UILabel     = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
+        noDataLabel.text          = "No result"
+        noDataLabel.textColor     = UIColor.black
+        noDataLabel.textAlignment = .center
+        tableView.backgroundView  = noDataLabel
+        tableView.separatorStyle  = .none
     }
 }
 
